deba1ba740fc3395e5de6adebf00829ea762eeef
ADai the Rocker <39561709+RockADai@users.noreply.github.com>
2018-07-15 22:07:43 +0800

    Update Prac2_5.c

--- Chapter2/Prac2_5.c
+++ Chapter2/Prac2_5.c
@@ -4,7 +4,7 @@
 若s1中不包含s2的字符则返回-1
 */
 int any(char s1[],char s2[]){
-    int i;
+    int i,j;
     for(i = 0;s1[i] != '\0';i++){
         for(j = 0;s2[j] != '\0';j++)
             if(s1[i] == s2[j])
